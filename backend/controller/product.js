3878db9ca16a5c77b4a07a692109081d06699fde
Mohammad <saifuddin.md.er@gmail.com>
2023-07-29 16:58:20 +0530

    added All products page and delete product

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { isSeller } = require('../middleware/auth');
 // const { isSeller, isAuthenticated, isAdmin } = require("../middleware/auth");
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const router = express.Router();
@@ -124,35 +125,37 @@ router.get(
   })
 );
 
-// // delete product of a shop
-// router.delete(
-//   '/delete-shop-product/:id',
-//   isSeller,
-//   catchAsyncErrors(async (req, res, next) => {
-//     try {
-//       const product = await Product.findById(req.params.id);
+// delete product of a shop
+router.delete(
+  '/delete-shop-product/:id',
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const productId = req.params.id;
+      const product = await Product.findByIdAndDelete(productId);
+      // const product = await Product.findById(req.params.id);
 
-//       if (!product) {
-//         return next(new ErrorHandler('Product is not found with this id', 404));
-//       }
+      if (!product) {
+        return next(new ErrorHandler('Product is not found with this id', 404));
+      }
 
-//       for (let i = 0; 1 < product.images.length; i++) {
-//         const result = await cloudinary.v2.uploader.destroy(
-//           product.images[i].public_id
-//         );
-//       }
+      // for (let i = 0; 1 < product.images.length; i++) {
+      //   const result = await cloudinary.v2.uploader.destroy(
+      //     product.images[i].public_id
+      //   );
+      // }
 
-//       await product.remove();
+      // await product.remove();
 
-//       res.status(201).json({
-//         success: true,
-//         message: 'Product Deleted successfully!',
-//       });
-//     } catch (error) {
-//       return next(new ErrorHandler(error, 400));
-//     }
-//   })
-// );
+      res.status(201).json({
+        success: true,
+        message: 'Product Deleted successfully!',
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
 
 // // get all products
 // router.get(
